a2ef1e7735861fc49506e87af9a542abae0c79de
Aimee Temple <52682021+AimeeT8@users.noreply.github.com>
2021-02-21 16:02:52 -0500

    Initial commit

---
+++ src/NameTag.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+var NameTag = (props) => (
+  <div className="name-tag">
+    <h3 className="title">Hello</h3>
+    <p className="subtitle">My name is</p>
+    <h2 className="name">{props.name}</h2>
+  </div>
+);
+
+export default NameTag;
---
+++ src/data.js
@@ -0,0 +1,6 @@
+export var names = [
+  { personname: "Chlo", id: "007" },
+  { personname: "Duke", id: "009" },
+  { personname: "Ozzie", id: "005" },
+  { personname: "Dorito", id: "002" }
+];
